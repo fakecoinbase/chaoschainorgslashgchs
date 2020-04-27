360fae85fead5715ba368ccf51d276ae878dc65c
Jackarain <jack.wgm@gmail.com>
2020-04-27 23:41:05 +0800

    chang datadir/eth to datadir/chs and protocolName to chs

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -46,7 +46,7 @@ import (
 )
 
 const (
-	clientIdentifier = "geth" // Client identifier to advertise over the network
+	clientIdentifier = "gchs" // Client identifier to advertise over the network
 )
 
 var (
